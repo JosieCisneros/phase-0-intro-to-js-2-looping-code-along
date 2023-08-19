d23b786e16b4dd785ec9c35850fff3be36dc849d
Josie Cisneros <josiecisneros1224@gmail.com>
2023-08-18 18:10:59 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = ["surprise"]
+
+function writeCards(names) {
+    let message = [] 
+    for( let i = 0; i < names.length; i++) {
+        message.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return message
+}
+console.log (writeCards(names))
+
+
+const number = "10"
+
+function countDown(number) {
+    let integer = 10
+    while (integer >= 0) {
+        console.log (integer--)
+    }
+    return integer
+}
+console.log (countDown(number))
